78f4b81b659263aa2fad3a4d7cf7272de0153a10
SamCabV <scabreravalencia@olin.edu>
2023-12-12 15:18:23 -0500

    clean up rep

--- cube_visualization/graph_try/node_graph.py
+++
@@ -1,8 +0,0 @@
-from pyvis.network import Network
-
-net = Network()
-net.add_node(1, label = "e")
-net.add_node(2, label = "F")
-net.add_edge(1,2,weight=0.87)
-net.toggle_physics(True)
-net.show("rubics_test1.html",notebook=False)
